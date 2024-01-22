d1ae0bc912317c8ed5fe15035c7b010e2f1e17ec
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2024-01-22 11:01:06 +0100

    update
    - start desktop versie
    - p tag gesloten

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// DropDOwn mobile
+
 // JS code has to wait until my HTML is fully loaded before it can trigger the JS code
 document.addEventListener('DOMContentLoaded' , function () {
     //use arrow-icon to show and hide elements
@@ -19,5 +21,30 @@ document.addEventListener('DOMContentLoaded' , function () {
         lineElement.classList.toggle('hidden');
 
         barLinksElement.classList.toggle('hidden');
+    });
+});
+
+// SIDEBAR DESKTOP
+document.addEventListener('DOMContentLoaded' , function () {
+    //use arrow-icon to show and hide elements
+    const sideBarText1 = document.querySelector('.sidebar-text');
+
+    const sideBarText2 = document.querySelector('.sidebar-text2');
+
+
+    const sideLink  = document.querySelector('.side-line');
+
+    // Click event Listener to the arrow-icon
+    sideBarText1.addEventListener('click', function () {
+        // toggle down and up on arrow-icon
+        sideBarText1.classList.toggle('sidebar-text');
+        console.log('clicked');
+
+        sideBarText2.classList.toggle('sidebar-text2');
+
+        // toggle 'hidden' class on the line and bar-links elements
+        sideBarText1.classList.toggle('hidden');
+
+        sideLink.classList.toggle('hidden');
     });
 });
