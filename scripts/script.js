75ee5daadf2d71c383be26e31b683af5db76ac1a
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2024-01-23 00:36:36 +0100

    updated  2.0
    added
    -tabs
    -external-link
    -table
    -design tokens
    -do and don't
    -positions
    -start estafette status

--- scripts/script.js
+++ scripts/script.js
@@ -48,3 +48,19 @@ document.addEventListener('DOMContentLoaded' , function () {
         sideLink.classList.toggle('hidden');
     });
 });
+
+// Tabs
+
+function languages(evt, lang) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(lang).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
